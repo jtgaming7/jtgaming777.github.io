9bce4d310f13d0853172cd2d73c65f1561ab76a8
jtgaming7 <149912786+jtgaming7@users.noreply.github.com>
2026-03-02 15:48:02 -0700

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,8 +1,8 @@
 const d = {
 	"list": [
-		/*=================================================================================*/
-		{
-			"vids": [
+			/*=================================================================================*/
+			{
+				"vids": [
 				{
 					"user": "JT",
 					"link": " https://www.youtube.com/watch?v=kLlV43vJ1SE",
@@ -18,184 +18,184 @@ const d = {
                        "percentToQualify": 46,
                        "verificationVid": "https://www.youtube.com/watch?v=kLlV43vJ1SE",
                        "key": 0
-		},
-		/*=================================================================================*/	
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Yatagarasu",
-                       "author": "Ben",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 45,
-                       "verificationVid": "https://youtu.be/-p8Z_NU2GB0?si=L3Mmwk_ud5uzXOMl",
-                       "key": 1
-            	},
-		
-		/*=================================================================================*/
-		{
-                       "vids": [
-
-		       ],
-                       "name": "Quantum Porcessing",
-                       "author": "Gabe",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 49,
-                       "verificationVid": "https://youtu.be/IaofML5KTdQ?si=jQ1Ig7Gzq-k7y1ZK",
-                       "key": 1
-                },
-		/*=================================================================================*/		
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Auditory Breaker",
-                       "author": "Andrew",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 45,
-                       "verificationVid": " ",
-                       "key": 1
-            	},
-		
-		/*=================================================================================*/		
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Ouroboros Startpos 2",
-                       "author": "JT",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 64,
-                       "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl",
-                       "key": 1
-                },
-	/*=================================================================================*/		
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Surge of the Shield",
-                       "author": "JT",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 51,
-                       "verificationVid": "https://www.youtube.com/watch?v=bvObypx7roQ&t=1s",
-                       "key": 1
-                },
-
-		/*=================================================================================*/		
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Shock Therapy",
-                       "author": "Ben",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 54,
-                       "verificationVid": "https://youtu.be/kS29Kmzw55o?si=NPSuQYsaP-e37Jf6",
-                       "key": 1
-                },
-	/*=================================================================================*/		
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Dolos",
-                       "author": "JT",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 55,
-                       "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl",
-                       "key": 1
-                },
-	/*=================================================================================*/		
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Phobos",
-                       "author": "Ben",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 52,
-                       "verificationVid": "https://youtu.be/KrCrLQz05KA?si=uMGyR-54Bhy0zp3r",
-                       "key": 1
-                },
-	/*=================================================================================*/
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "WHITEKNUCKLES",
-                       "author": "JT",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 52,
-                       "verificationVid": "https://youtu.be/2ImqD2LMlqs?si=60LCz3xXHz3ZM6Y8",
-                       "key": 1
-                },
-	/*=================================================================================*/
-		{
-                       "vids": [
-						   {
-					"user": "JT",
-					"link": " https://youtu.be/r7m4jtjTygk?si=_NZ302Cl-UXCWCvz",
-					"percent": 100
-					"hz": "240hz" 
-				},
-						   {
-					"user": "Andrew",
-					"link": " ",
-					"percent": 100
-					"hz": "240hz" 
-				},
-						   {
-					"user": "Ben",
-					"link": " https://youtu.be/xAwz8OIA-_Q?si=NpYW1G4YzweSyBmk",
-					"percent": 100
-					"hz": "240hz" 
-				},
-			    
-		       ],
-                       "name": "Bloodbath",
-                       "author": "Gabe",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 42,
-                       "verificationVid": "https://youtu.be/L2aKNNaGkRY",
-                       "key": 1
-                },
-	/*=================================================================================*/
-		{
-                       "vids": [
-			    
-		       ],
-                       "name": "Aftermath",
-                       "author": "Andrew",
-                       "more": "none",
-                       "id": 63009517,
-                       "pass": "Free to copy",
-                       "percentToQualify": 47,
-                       "verificationVid": " ",
-                       "key": 1
-                },
-	/*=================================================================================*/
+			},
+			/*=================================================================================*/	
+			{
+	                       "vids": [
+				    
+			       ],
+	                       "name": "Yatagarasu",
+	                       "author": "Ben",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 45,
+	                       "verificationVid": "https://youtu.be/-p8Z_NU2GB0?si=L3Mmwk_ud5uzXOMl",
+	                       "key": 1
+	            	},
 			
+			/*=================================================================================*/
+			{
+	                       "vids": [
+	
+			       ],
+	                       "name": "Quantum Porcessing",
+	                       "author": "Gabe",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 49,
+	                       "verificationVid": "https://youtu.be/IaofML5KTdQ?si=jQ1Ig7Gzq-k7y1ZK",
+	                       "key": 1
+	                },
+			/*=================================================================================*/		
+			{
+	                       "vids": [
+				    
+			       ],
+	                       "name": "Auditory Breaker",
+	                       "author": "Andrew",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 45,
+	                       "verificationVid": "https://www.youtube.com/watch?v=bvObypx7roQ&t=1s",
+	                       "key": 1
+	            	},
+			
+			/*=================================================================================*/		
+			{
+	                "vids": [
+				    
+			       ],
+	                       "name": "Ouroboros Startpos 2",
+	                       "author": "JT",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 64,
+	                       "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl",
+	                       "key": 1
+	                },
+		/*=================================================================================*/		
+			{
+	                "vids": [
+				    
+			       ],
+	                       "name": "Surge of the Shield",
+	                       "author": "JT",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 51,
+	                       "verificationVid": "https://www.youtube.com/watch?v=bvObypx7roQ&t=1s",
+	                       "key": 1
+	                },
+	
+			/*=================================================================================*/		
+			{
+	                "vids": [
+				    
+			       ],
+	                       "name": "Shock Therapy",
+	                       "author": "Ben",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 54,
+	                       "verificationVid": "https://youtu.be/kS29Kmzw55o?si=NPSuQYsaP-e37Jf6",
+	                       "key": 1
+	                },
+		/*=================================================================================*/		
+			{
+	                "vids": [
+				    
+			       ],
+	                       "name": "Dolos",
+	                       "author": "JT",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 55,
+	                       "verificationVid": "https://youtu.be/CT84uKtqhYo?si=utIC9-V7tRXxmkbl",
+	                       "key": 1
+	                },
+		/*=================================================================================*/		
+			{
+	                 "vids": [
+				    
+			       ],
+	                       "name": "Phobos",
+	                       "author": "Ben",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 52,
+	                       "verificationVid": "https://youtu.be/KrCrLQz05KA?si=uMGyR-54Bhy0zp3r",
+	                       "key": 1
+	                },
+		/*=================================================================================*/
+			{
+	                "vids": [
+				    
+			       ],
+	                       "name": "WHITEKNUCKLES",
+	                       "author": "JT",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 52,
+	                       "verificationVid": "https://youtu.be/2ImqD2LMlqs?si=60LCz3xXHz3ZM6Y8",
+	                       "key": 1
+	                },
+		/*=================================================================================*/
+			{
+	             "vids": [
+							   {
+						"user": "JT",
+						"link": " https://youtu.be/r7m4jtjTygk?si=_NZ302Cl-UXCWCvz",
+						"percent": 100
+						"hz": "240hz" 
+								},
+							   {
+						"user": "Andrew",
+						"link": " ",
+						"percent": 100
+						"hz": "240hz" 
+					},
+							   {
+						"user": "Ben",
+						"link": " https://youtu.be/xAwz8OIA-_Q?si=NpYW1G4YzweSyBmk",
+						"percent": 100
+						"hz": "240hz" 
+					},
+				    
+			       ],
+	                       "name": "Bloodbath",
+	                       "author": "Gabe",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 42,
+	                       "verificationVid": "https://youtu.be/L2aKNNaGkRY",
+	                       "key": 1
+	                },
+		/*=================================================================================*/
+			{
+	                       "vids": [
+				    
+			       ],
+	                       "name": "Aftermath",
+	                       "author": "Andrew",
+	                       "more": "none",
+	                       "id": 63009517,
+	                       "pass": "Free to copy",
+	                       "percentToQualify": 47,
+	                       "verificationVid": " ",
+	                       "key": 1
+	                },
+		/*=================================================================================*/
+				
 	],
 	"version": [
 		1,
@@ -205,6 +205,7 @@ const d = {
 };
 
 const list = d.list;const version = d.version;
+
 
 
 
